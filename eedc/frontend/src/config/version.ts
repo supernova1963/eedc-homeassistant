260ea6c8f2c546ed8f297d75e610bd6c1da5dddd
supernova1963 <25952021+supernova1963@users.noreply.github.com>
2026-04-09 12:19:58 +0200

    release: v3.11.17

--- eedc/frontend/src/config/version.ts
+++ eedc/frontend/src/config/version.ts
@@ -3,7 +3,7 @@
  * Hier wird die Version nur einmal definiert und überall verwendet
  */
 
-export const APP_VERSION = '3.11.16'
+export const APP_VERSION = '3.11.17'
 export const APP_NAME = 'eedc'
 export const APP_FULL_NAME = 'Energie Effizienz Data Center'
 
