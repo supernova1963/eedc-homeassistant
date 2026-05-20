79e0b13da2db8e6b085c40872ce14e3e2da7702d
supernova1963 <25952021+supernova1963@users.noreply.github.com>
2026-05-20 15:00:50 +0200

    release: v3.31.6

--- eedc/frontend/dist/assets/DesignPreview-Ys1g-uqs.js
+++
@@ -1,1 +0,0 @@
-import"./index-BLYZuyg7.js";import"./vendor-BDzNU4NU.js";import"./charts-Cite5-MP.js";function i(){return null}export{i as default};
---
+++ eedc/frontend/dist/assets/DesignPreview-dsnRdz_a.js
@@ -0,0 +1,1 @@
+import"./index-BsYL2SaK.js";import"./vendor-BDzNU4NU.js";import"./charts-VYenJXAr.js";function i(){return null}export{i as default};
